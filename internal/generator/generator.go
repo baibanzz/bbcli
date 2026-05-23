05199c186ac8f30b28c7be2375aa83f3fe04b768
jxc1690 <jxc1690@qq.com>
2026-05-23 21:38:50 +0700

    init2

--- internal/generator/generator.go
+++ internal/generator/generator.go
@@ -3,6 +3,7 @@ package generator
 import (
 	"bbcli/internal/config"
 	"bytes"
+	"embed"
 	"fmt"
 	"os"
 	"path/filepath"
@@ -10,18 +11,68 @@ import (
 	"text/template"
 )
 
+//go:embed *.tmpl
+var templateFiles embed.FS
+
 // Generator 代码生成器
 type Generator struct {
-	cfg *config.BBConfig
-	dir string
+	cfg       *config.BBConfig
+	dir       string
+	templates map[string]*template.Template
 }
 
 // NewGenerator 创建生成器实例
 func NewGenerator(dir string, cfg *config.BBConfig) *Generator {
-	return &Generator{
-		cfg: cfg,
-		dir: dir,
+	g := &Generator{
+		cfg:       cfg,
+		dir:       dir,
+		templates: make(map[string]*template.Template),
 	}
+	g.loadTemplates()
+	return g
+}
+
+// loadTemplates 加载所有模板文件
+func (g *Generator) loadTemplates() {
+	funcMap := template.FuncMap{
+		"lower":     strings.ToLower,
+		"firstChar": firstChar,
+		"restChars": restChars,
+	}
+
+	templates := []string{
+		"gomod.tmpl",
+		"main.tmpl",
+		"router.tmpl",
+		"handler.tmpl",
+		"middleware.tmpl",
+		"config.tmpl",
+	}
+
+	for _, name := range templates {
+		tmpl, err := template.New(name).Funcs(funcMap).ParseFS(templateFiles, name)
+		if err != nil {
+			fmt.Printf("加载模板 %s 失败: %v\n", name, err)
+			continue
+		}
+		g.templates[name] = tmpl
+	}
+}
+
+// firstChar 返回首字符小写
+func firstChar(s string) string {
+	if len(s) == 0 {
+		return ""
+	}
+	return strings.ToLower(s[:1])
+}
+
+// restChars 返回剩余字符
+func restChars(s string) string {
+	if len(s) <= 1 {
+		return ""
+	}
+	return s[1:]
 }
 
 // fileStrategy 定义文件处理策略
@@ -30,7 +81,6 @@ type fileStrategy int
 const (
 	strategyOverwrite fileStrategy = iota
 	strategyIgnore
-	strategyCreate
 )
 
 // shouldHandle 决定是否处理文件
@@ -40,10 +90,6 @@ func (g *Generator) shouldHandle(relPath string, strategy fileStrategy) bool {
 		return true
 	case strategyIgnore:
 		_, err := os.Stat(filepath.Join(g.dir, relPath))
-		return os.IsNotExist(err)
-	case strategyCreate:
-		fullPath := filepath.Join(g.dir, relPath)
-		_, err := os.Stat(fullPath)
 		return os.IsNotExist(err)
 	default:
 		return false
@@ -97,30 +143,6 @@ func (g *Generator) Generate() error {
 	return nil
 }
 
-// templateData 用于模板渲染的数据
-type templateData struct {
-	Project        config.ProjectConfig
-	Routes         []config.RouteConfig
-	Middleware     []string
-	Handler        string
-	MiddlewareName string
-}
-
-// renderTemplate 渲染模板
-func (g *Generator) renderTemplate(tmplStr string, data interface{}) (string, error) {
-	tmpl, err := template.New("").Parse(tmplStr)
-	if err != nil {
-		return "", err
-	}
-
-	var buf bytes.Buffer
-	if err := tmpl.Execute(&buf, data); err != nil {
-		return "", err
-	}
-
-	return buf.String(), nil
-}
-
 // ensureDir 确保目录存在
 func (g *Generator) ensureDir(path string) error {
 	fullPath := filepath.Join(g.dir, path)
@@ -128,245 +150,138 @@ func (g *Generator) ensureDir(path string) error {
 }
 
 // writeFile 写入文件
-func (g *Generator) writeFile(relPath, content string) error {
+func (g *Generator) writeFile(relPath string, content []byte) error {
 	fullPath := filepath.Join(g.dir, relPath)
-	return os.WriteFile(fullPath, []byte(content), 0644)
+	return os.WriteFile(fullPath, content, 0644)
 }
 
-// 生成 go.mod
+// executeTemplate 执行模板
+func (g *Generator) executeTemplate(tmplName string, data interface{}) ([]byte, error) {
+	tmpl, ok := g.templates[tmplName]
+	if !ok {
+		return nil, fmt.Errorf("模板 %s 不存在", tmplName)
+	}
+
+	var buf bytes.Buffer
+	if err := tmpl.Execute(&buf, data); err != nil {
+		return nil, err
+	}
+
+	return buf.Bytes(), nil
+}
+
+// generateGoMod 生成 go.mod
 func (g *Generator) generateGoMod() error {
-	content := fmt.Sprintf(`module %s
+	data := struct {
+		Project config.ProjectConfig
+	}{
+		Project: g.cfg.Project,
+	}
 
-go 1.21
+	content, err := g.executeTemplate("gomod.tmpl", data)
+	if err != nil {
+		return err
+	}
 
-require (
-	github.com/gin-gonic/gin v1.9.1
-	gorm.io/gorm v1.25.2
-)
-
-require (
-	github.com/bytedance/sonic v1.9.1 // indirect
-	github.com/chenzhuoyu/base64x v0.0.0-20221115062448-fe3a3abad311 // indirect
-	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/go-playground/locales v0.14.1 // indirect
-	github.com/go-playground/universal-translator v0.18.1 // indirect
-	github.com/go-playground/validator/v10 v10.14.0 // indirect
-	github.com/goccy/go-json v0.10.2 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
-	github.com/jinzhu/now v1.1.5 // indirect
-	github.com/json-iterator/go v1.1.12 // indirect
-	github.com/klauspost/cpuid/v2 v2.2.4 // indirect
-	github.com/leodido/go-urn v1.2.4 // indirect
-	github.com/mattn/go-isatty v0.0.19 // indirect
-	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
-	github.com/modern-go/reflect2 v1.0.2 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.8 // indirect
-	github.com/twitchyliquid64/golang-asm v0.15.1 // indirect
-	github.com/ugorji/go/codec v1.2.11 // indirect
-	golang.org/x/arch v0.3.0 // indirect
-	golang.org/x/crypto v0.9.0 // indirect
-	golang.org/x/net v0.10.0 // indirect
-	golang.org/x/sys v0.8.0 // indirect
-	golang.org/x/text v0.9.0 // indirect
-	google.golang.org/protobuf v1.30.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
-`, g.cfg.Project.Name)
 	return g.writeFile("go.mod", content)
 }
 
-// 生成 main.go
+// generateMainGo 生成 main.go
 func (g *Generator) generateMainGo() error {
-	content := `package main
-
-import (
-	"fmt"
-	"log"
-	"%[1]s/config"
-	"%[1]s/router"
-)
-
-func main() {
-	// 加载配置
-	cfg := config.Load()
-	
-	// 创建 Gin 实例
-	r := router.Setup(cfg)
-	
-	// 启动服务器
-	addr := fmt.Sprintf(":%d", cfg.Project.Port)
-	log.Printf("服务器启动在 http://localhost%s", addr)
-	if err := r.Run(addr); err != nil {
-		log.Fatalf("服务器启动失败: %v", err)
+	data := struct {
+		Project config.ProjectConfig
+	}{
+		Project: g.cfg.Project,
 	}
-}
-`
-	content = fmt.Sprintf(content, g.cfg.Project.Name)
+
+	content, err := g.executeTemplate("main.tmpl", data)
+	if err != nil {
+		return err
+	}
+
 	return g.writeFile("main.go", content)
 }
 
-// 生成 router.go
+// generateRouter 生成 router.go
 func (g *Generator) generateRouter() error {
 	if err := g.ensureDir("router"); err != nil {
 		return err
 	}
 
-	var buf bytes.Buffer
-	buf.WriteString("package router\n\n")
-	buf.WriteString("import (\n")
-	buf.WriteString("\t\"" + g.cfg.Project.Name + "/config\"\n")
-	buf.WriteString("\t\"" + g.cfg.Project.Name + "/handler\"\n")
-
-	// 导入 middleware 包
-	if len(g.cfg.Middlewares) > 0 {
-		buf.WriteString("\t\"" + g.cfg.Project.Name + "/middleware\"\n")
+	data := struct {
+		Project     config.ProjectConfig
+		Routes      []config.RouteConfig
+		Middlewares []string
+	}{
+		Project:     g.cfg.Project,
+		Routes:      g.cfg.Routes,
+		Middlewares: g.cfg.Middlewares,
 	}
 
-	buf.WriteString("\t\"github.com/gin-gonic/gin\"\n")
-	buf.WriteString(")\n\n")
-
-	// Setup 函数
-	buf.WriteString("func Setup(cfg *config.Config) *gin.Engine {\n")
-	buf.WriteString("\tr := gin.Default()\n\n")
-
-	// 注册中间件
-	for _, mw := range g.cfg.Middlewares {
-		mwVar := strings.ToLower(mw[0:1]) + mw[1:]
-		buf.WriteString(fmt.Sprintf("\tr.Use(middleware.%s())\n", mwVar))
-	}
-	buf.WriteString("\n")
-
-	// 注册路由
-	for _, route := range g.cfg.Routes {
-		handlerVar := strings.ToLower(route.Handler[0:1]) + route.Handler[1:]
-		buf.WriteString(fmt.Sprintf("\tr.%s(\"%s\", handler.%s)\n",
-			route.Method, route.Path, handlerVar))
+	content, err := g.executeTemplate("router.tmpl", data)
+	if err != nil {
+		return err
 	}
 
-	buf.WriteString("\n\treturn r\n}\n")
-
-	return g.writeFile("router/router.go", buf.String())
+	return g.writeFile("router/router.go", content)
 }
 
-// 生成 handler
+// generateHandler 生成 handler
 func (g *Generator) generateHandler(handlerName string) error {
 	if err := g.ensureDir("handler"); err != nil {
 		return err
 	}
 
-	data := templateData{
+	data := struct {
+		Handler string
+	}{
 		Handler: handlerName,
 	}
 
-	tmpl := `package handler
-
-import (
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
-
-func {{.Handler}}(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"message": "Hello, World!",
-	})
-}
-`
-
-	content, err := g.renderTemplate(tmpl, data)
+	content, err := g.executeTemplate("handler.tmpl", data)
 	if err != nil {
 		return err
 	}
 
 	// 小写处理文件名
-	fileName := strings.ToLower(handlerName[0:1]) + handlerName[1:] + ".go"
+	fileName := firstChar(handlerName) + restChars(handlerName) + ".go"
 	return g.writeFile("handler/"+fileName, content)
 }
 
-// 生成 middleware
+// generateMiddleware 生成 middleware
 func (g *Generator) generateMiddleware(mwName string) error {
 	if err := g.ensureDir("middleware"); err != nil {
 		return err
 	}
 
-	data := templateData{
-		MiddlewareName: mwName,
+	data := struct {
+		Name string
+	}{
+		Name: mwName,
 	}
 
-	tmpl := `package middleware
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func {{.MiddlewareName}}() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		// TODO: 实现中间件逻辑
-		c.Next()
-	}
-}
-`
-
-	content, err := g.renderTemplate(tmpl, data)
+	content, err := g.executeTemplate("middleware.tmpl", data)
 	if err != nil {
 		return err
 	}
 
 	// 小写处理文件名
-	fileName := strings.ToLower(mwName[0:1]) + mwName[1:] + ".go"
+	fileName := firstChar(mwName) + restChars(mwName) + ".go"
 	return g.writeFile("middleware/"+fileName, content)
 }
 
-// 生成数据库配置
+// generateDatabase 生成数据库配置
 func (g *Generator) generateDatabase() error {
 	if err := g.ensureDir("config"); err != nil {
 		return err
 	}
 
-	var buf bytes.Buffer
-	buf.WriteString("package config\n\n")
+	data := g.cfg.Project
 
-	switch g.cfg.Project.Database.Type {
-	case "mysql":
-		buf.WriteString("var DB *gorm.DB\n\n")
-		buf.WriteString("func InitDB() error {\n")
-		buf.WriteString("\tdsn := \"root:@tcp(localhost:3306)/myapp?charset=utf8mb4&parseTime=True&loc=Local\"\n")
-		buf.WriteString("\tdb, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})\n")
-		buf.WriteString("\tif err != nil {\n")
-		buf.WriteString("\t\treturn err\n")
-		buf.WriteString("\t}\n")
-		buf.WriteString("\tDB = db\n")
-		buf.WriteString("\treturn nil\n}\n")
-
-	case "postgres":
-		buf.WriteString("var DB *gorm.DB\n\n")
-		buf.WriteString("func InitDB() error {\n")
-		buf.WriteString("\tdsn := \"host=localhost user=postgres password= dbname=myapp port=5432 sslmode=disable\"\n")
-		buf.WriteString("\tdb, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})\n")
-		buf.WriteString("\tif err != nil {\n")
-		buf.WriteString("\t\treturn err\n")
-		buf.WriteString("\t}\n")
-		buf.WriteString("\tDB = db\n")
-		buf.WriteString("\treturn nil\n}\n")
+	content, err := g.executeTemplate("config.tmpl", data)
+	if err != nil {
+		return err
 	}
 
-	buf.WriteString("func Load() *Config {\n")
-	buf.WriteString("\treturn &Config{\n")
-	buf.WriteString("\t\tProject: ProjectConfig{\n")
-	buf.WriteString(fmt.Sprintf("\t\t\tName: \"%s\",\n", g.cfg.Project.Name))
-	buf.WriteString(fmt.Sprintf("\t\t\tPort: %d,\n", g.cfg.Project.Port))
-	buf.WriteString("\t\t\tDatabase: DatabaseConfig{\n")
-	buf.WriteString(fmt.Sprintf("\t\t\t\tType: \"%s\",\n", g.cfg.Project.Database.Type))
-	buf.WriteString(fmt.Sprintf("\t\t\t\tHost: \"%s\",\n", g.cfg.Project.Database.Host))
-	buf.WriteString(fmt.Sprintf("\t\t\t\tPort: %d,\n", g.cfg.Project.Database.Port))
-	buf.WriteString(fmt.Sprintf("\t\t\t\tUser: \"%s\",\n", g.cfg.Project.Database.User))
-	buf.WriteString(fmt.Sprintf("\t\t\t\tPassword: \"%s\",\n", g.cfg.Project.Database.Password))
-	buf.WriteString(fmt.Sprintf("\t\t\t\tDatabase: \"%s\",\n", g.cfg.Project.Database.Database))
-	buf.WriteString("\t\t\t},\n")
-	buf.WriteString("\t\t},\n")
-	buf.WriteString("\t}\n")
-	buf.WriteString("}\n")
-
-	return g.writeFile("config/config.go", buf.String())
+	return g.writeFile("config/config.go", content)
 }
